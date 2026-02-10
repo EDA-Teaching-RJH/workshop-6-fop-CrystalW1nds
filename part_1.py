e5d466269533dd313c7e19ea2890fa308ce2bbca
CrystalW1nds <hf280@kent.ac.uk>
2026-02-10 12:24:15 +0000

    part 1 finished

--- part_1.py
+++ part_1.py
@@ -1,1 +1,27 @@
+def main():
+    sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+    finalIdx = len(sample_bay) - 1
+    new_findings = []
 
+    print(sample_bay[0])
+    print(sample_bay[finalIdx])
+
+    for i in range(len(sample_bay)):
+        print("Transmitting data for: ", sample_bay[i])
+
+    for i in range(3):
+        new = input("Please enter a new rock type found: ")
+        new_findings.append(new)
+
+    for i in range(len(new_findings)):
+        print(new_findings[i])
+
+    for i in range(len(sample_bay)):
+        if sample_bay[i] == "Dust":
+            sample_bay.pop(i)
+
+    for i in range(len(sample_bay)):
+        print(sample_bay[i])
+
+
+main()
