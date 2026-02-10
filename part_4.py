465e70016aceefc05e92d2b432259c7fa46e34bc
CrystalW1nds <hf280@kent.ac.uk>
2026-02-10 13:20:33 +0000

    finished part 4

--- part_4.py
+++ part_4.py
@@ -1,1 +1,23 @@
+def main():
+    travel_log = []
 
+    while True:
+
+        try:
+            angle = int(input("Sensor reading (slope angle): "))
+
+        except:
+            print("Sensor Glitch.")
+
+        if angle > 45:
+            print("CRITICAL TILT! HALTING.")
+            break
+
+        travel_log.append(angle)
+        print("Path stable. Moving forward.")
+
+    print("Mission Terminated.")
+    print("Total steps taken: ", len(travel_log))
+    print(travel_log)
+
+main()
