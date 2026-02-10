f13020f507821cd47f2eb42166abe5224669a3e0
CrystalW1nds <hf280@kent.ac.uk>
2026-02-10 13:12:03 +0000

    finished part 3

--- part_3.py
+++ part_3.py
@@ -1,1 +1,28 @@
+def main():
+    try:
+        speed = int(input("Enter motor speed: "))
+        print("Speed set to ", speed)
 
+    except:
+        print("Error: Corrupted Signal. Maintaining current speed.")
+
+    xCoord = get_coordinate()
+
+def get_coordinate():
+    while True:
+        
+        try:
+            xCoord = int(input("Please enter a valid X-Coordinate: "))
+
+        except:
+            print("Invalid")
+
+        if xCoord > 100 or xCoord < -100:
+            print("Invalid")
+
+        else:
+            break
+
+    return xCoord   
+
+main()
